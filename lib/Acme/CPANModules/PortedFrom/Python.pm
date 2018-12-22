da28b13b662eb55caab149a0194c20c121d01367
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-12-22 08:25:36 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/Python.pm
@@ -0,0 +1,26 @@
+package Acme::CPANModules::PortedFrom::Python;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "Python libraries",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'Docopt',
+            python_package => 'docopt',
+            tags => ['cli'],
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
