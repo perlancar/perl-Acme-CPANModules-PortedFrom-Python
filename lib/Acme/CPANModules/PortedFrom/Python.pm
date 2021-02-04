2a073e8cb9db9ec6da6bda04caf30b2ae6c55289
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-05 06:38:02 +0700

    Grammar

--- lib/Acme/CPANModules/PortedFrom/Python.pm
+++ lib/Acme/CPANModules/PortedFrom/Python.pm
@@ -32,9 +32,9 @@ _
             description => <<'_',
 
 From Plack's documentation: "Plack is like Ruby's Rack or Python's Paste for
-WSGI." Plack and PSGI was created by MIYAGAWA in 2009 and is inspired by both
+WSGI." Plack and PSGI were created by MIYAGAWA in 2009 and were inspired by both
 Python's WSGI specification (hence the dual specification-implementation split)
-and Plack (hence the name).
+and Ruby's Plack (hence the name).
 
 _
         },
